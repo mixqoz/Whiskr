fbf493ecd9acf4f1707e89f108dbc410e8340aa0
mixqoz <unknownmixqo@gmail.com>
2025-11-24 12:57:57 +0100

    Sign up page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Whiskr
 An app for couples to connect and ask themselves and each other peculiar questions they usually don't. 
+
+# Sources
+Login & Signup portal made by https://www.youtube.com/watch?v=bVl5_UdcAy0
--- app.py
+++ app.py
@@ -17,6 +17,17 @@ except:
 def index():
 	return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
